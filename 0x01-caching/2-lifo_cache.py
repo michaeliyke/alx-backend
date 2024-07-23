49b4dd9bb7475f40a017dd21800f02d7b4ed67a5
Michael Iyke <ymichaelc@gmail.com>
2024-07-23 16:10:45 +0100

    Task 02: Implement the LIFO experiation policy

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+"""Module for the LIFO cache task"""
+from base import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """LIFO cache class"""
+
+    def __init__(self):
+        """Constructor"""
+        super().__init__()
+        self.keys_stack = []
+
+    def put(self, key, item):
+        """Method that add key:value to the dictionary"""
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                return
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                item_key = self.keys_stack.pop()
+                del self.cache_data[item_key]
+                print("DISCARD:", item_key)
+            self.cache_data[key] = item
+            self.keys_stack.append(key)
+
+    def get(self, key):
+        """Method that return the value of the key"""
+        return self.cache_data.get(key, None)
