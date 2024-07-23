a6de65f463bc5a35cd98f2143aedf04f4f6a8e10
Michael Iyke <ymichaelc@gmail.com>
2024-07-23 15:28:59 +0100

    Task 00: Basic caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""Basic caching module to start with"""
+from base import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache class inherits from BaseCaching"""
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
