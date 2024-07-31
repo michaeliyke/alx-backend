5e68b660a54848ec0ee56315c558e6f9421ae754
Michael Iyke <ymichaelc@gmail.com>
2024-07-31 12:47:26 +0100

    Correct

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -20,6 +20,7 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale() -> Optional[str]:
+    """Get locale from request"""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
