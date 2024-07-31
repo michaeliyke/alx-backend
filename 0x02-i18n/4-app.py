b5ff3dac3abc43122e22e24e4a8c35820591b782
Michael Iyke <ymichaelc@gmail.com>
2024-07-31 19:52:48 +0100

    Task 04

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,2 +1,45 @@
 #!/usr/bin/env python3
 """Internationlization (i18n) and localization (l10n) with Flask"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _  # type: ignore
+from typing import List, Union, Optional
+import logging
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Babel configuration class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> Optional[str]:
+    """Get locale from request"""
+    locale = request.args.get("locale")
+    logging.debug(f"Locale from request: {locale}")
+    print(type(locale))
+    if locale and locale in ['en', 'fr']:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """GET /
+    Return: 1-index.html
+    """
+    return render_template(
+        "1-index.html",
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000, debug=True)
+    # command to run: python3 -m 0-app
