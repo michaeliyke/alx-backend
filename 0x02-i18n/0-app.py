2eaebfef9c88754dd1dae7c1535da676303aeb2f
Michael Iyke <ymichaelc@gmail.com>
2024-07-31 12:47:26 +0100

    Correct

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,28 +1,8 @@
 #!/usr/bin/env python3
 """Internationlization (i18n) and localization (l10n) with Flask"""
-from flask import Flask, render_template, request, g
-from flask_babel import Babel, _  # type: ignore
-from typing import List, Union, Optional
+from flask import Flask, render_template
 
 app = Flask(__name__)
-babel = Babel(app)
-
-
-class Config:
-    """Babel configuration class"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
-
-
-@babel.localeselector
-def get_locale() -> Optional[str]:
-    """Get locale from request"""
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
-
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index() -> str:
