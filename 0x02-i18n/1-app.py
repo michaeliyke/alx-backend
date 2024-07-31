ec66df6f379286ccd7705030a0965266f7558aee
Michael Iyke <ymichaelc@gmail.com>
2024-07-31 13:07:38 +0100

    Correct

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,15 +8,6 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config:
-    """Babel configuration class for app"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
-
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index() -> str:
