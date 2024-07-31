ef8ef24bddcabaa3928ce4fafa52cfe763e4aaa8
Michael Iyke <ymichaelc@gmail.com>
2024-07-31 13:07:38 +0100

    Correct

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -18,18 +18,13 @@ class Config:
 app.config.from_object(Config)
 
 
-# @babel.localeselector
-# def get_locale() -> Optional[str]:
-#     """Get locale from request"""
-#     return request.accept_languages.best_match(app.config["LANGUAGES"])
-
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index() -> str:
     """GET /
     Return: 1-index.html
     """
-    return render_template("0-index.html")
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
