86e20d410ce0f35a6730edc48c67ed2ed803c288
Michael Iyke <ymichaelc@gmail.com>
2024-07-31 13:07:38 +0100

    Correct

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,29 +1,4 @@
 #!/usr/bin/env python3
-"""Internationlization (i18n) and localization (l10n) with Flask"""
-
-import flask
-from flask_babel import Babel # type: ignore
-
-app = flask.Flask(__name__)
-babel = Babel(app)
-
-
-class Config:
-    """Babel configuration class for app"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
-app.config.from_object(Config)
-
-
-@app.route("/", methods=["GET"], strict_slashes=False)
-def index() -> str:
-    """Returns the index page for the app"""
-    return flask.render_template("1-index.html")
-
-
-if __name__ == "__main__":
-    app.run(host="localhost", port=5000, debug=True)
-    # command to run: python3 -m 0-app
+"""
+Internationlization (i18n) and localization (l10n) with Flask
+"""
