7e24a6b476ab1abc24ad8e95c0a24d6818144cdc
Michael Iyke <ymichaelc@gmail.com>
2024-08-28 19:27:34 +0100

    Task 04: Using HSET for hash type

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,27 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+
+client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+client.hset('HolbertonSchools', 'New York', 20, redis.print);
+client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+
+client.hgetall('HolbertonSchools', (error, result) => {
+  if (error) {
+    console.log(`Error retrieving hash: ${error.message}`);
+  } else {
+    console.log(result);
+  }
+});
