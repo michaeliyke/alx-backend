802eb6d175a96892a7f975c1eb3b37fcae38f211
Michael Iyke <ymichaelc@gmail.com>
2024-08-28 16:41:19 +0100

    Task 02: Async getting

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,31 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+const getAsync = promisify(client.get).bind(client);
+
+async function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  try {
+    const value = await getAsync(schoolName);
+    console.log(`${value}`);
+  } catch (error) {
+    console.log(`Error retrieving value for key ${schoolName}: ${error.message}`);
+  }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
